6850fb7fa0ec04913262e881930745e663b840e8
user <user@system>
2015-10-29 21:20:16 +1300

    initial commit.

---
+++ myutils/plotting.py
@@ -0,0 +1,20 @@
+import pylab as pl
+import sys
+import os
+
+plot_count = 0
+def plot_out(figsize=None, dpi=60, newline=True, print_out=True):
+    global plot_count
+    filename = "plot_{}_{:03d}.png".format(os.path.basename(sys.argv[0]), plot_count)
+    plot_count += 1
+
+    old_size = pl.gcf().get_size_inches()
+    if figsize:
+        pl.gcf().set_size_inches(*figsize)
+        
+    pl.savefig(filename, dpi=dpi)
+    if print_out:
+        print "<{}>{}".format(filename, '\n' if newline else '')
+
+    pl.gcf().set_size_inches(old_size)
+    pl.clf()
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='myutils',
+      version='1.0',
+      description='My python utilities',
+      author='Vaughn Wood',
+      author_email='vwood@vwood.org',
+      url='',
+      packages=['myutils'],
+    )
+
